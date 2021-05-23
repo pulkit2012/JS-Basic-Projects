a44f47baf7c444d3bbb939dfbea1684d2a24c4b4
Pulkit Varshney <pulkitvarshney2012@gmail.com>
2021-05-23 13:49:18 +0530

    Color Flipper added

--- color-flipper/setup/hex.js
+++ color-flipper/setup/hex.js
@@ -1,1 +1,12 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', () => {
+  let val = '#'
+  for (let i = 0; i < 6; i++) {
+    val += hex[Math.floor(Math.random() * hex.length)]
+  }
+  document.body.style.backgroundColor = val
+  color.textContent = val
+})
